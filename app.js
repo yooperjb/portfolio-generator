3e308fe533172816433b2ca13691666118bbbdcd
Pedal <jmb181@humboldt.edu>
2020-11-09 23:13:18 -0800

    installed inquirer and wrote user input

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-// import modules
-const fs = require("fs");
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-// assinment destructuring
-const [name, github] = profileDataArgs;
+// // import modules
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const generatePage = require('./src/page-template.js');
 
 
-fs.writeFile('index.html', generatePage(name,github), err => {
-  if (err) throw new Error (err);
+// const pageHTML = generatePage(name,github);
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-})
+
+// fs.writeFile('index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
