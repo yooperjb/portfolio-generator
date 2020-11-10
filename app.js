b9775be01bf51499df913843f83aaca4cd2dfc5c
Pedal <jmb181@humboldt.edu>
2020-11-09 19:10:23 -0800

    html file generation

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+// import modules
+const fs = require("fs");
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
-  
-printProfileData(profileDataArgs);
+// assinment destructuring
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('index.html', generatePage(name,github), err => {
+  if (err) throw new Error (err);
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+})
